e7610c2d7bd062be7f761fe3ca4582bb87c79541
italo <Ital0Alve5>
2024-09-10 10:00:33 -0300

    ref: separar itens compartilhados por contexto

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { CardComponent } from '@/shared/UI/card/card.component';
-import { UserIcon } from '@/shared/UI/icons/user.icon';
-import { LockIcon } from '@/shared/UI/icons/lock.icon';
-import { InputComponent } from '@/shared/UI/input/input.component';
+import { CardComponent } from '@/shared/ui/card/card.component';
+import { UserIcon } from '@/shared/ui/icons/user.icon';
+import { LockIcon } from '@/shared/ui/icons/lock.icon';
+import { InputComponent } from '@/shared/ui/input/input.component';
 
 @Component({
   selector: 'app-login',

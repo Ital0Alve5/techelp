f11502ca82d68a63960e4c8e71936e5c15d190d1
YasmimZedelinski-Atlas <yasmim.zedelinski@atlastechnol.com>
2024-09-12 15:45:07 -0300

    fix: styles sing-up card

--- frontend/src/app/features/sing-up/sing-up.component.ts
+++ frontend/src/app/features/sing-up/sing-up.component.ts
@@ -73,7 +73,7 @@ export class SingUpComponent {
     },
     number: {
       value: '',
-      type: 'number',
+      type: 'text',
       validation: {
         error: false,
         message: '',

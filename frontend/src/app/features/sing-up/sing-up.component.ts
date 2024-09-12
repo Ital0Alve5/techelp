9b94f8c823aedb12d689d4505cc8b87a51c8d8b8
YasmimZedelinski-Atlas <yasmim.zedelinski@atlastechnol.com>
2024-09-12 14:30:09 -0300

    feat: add component for buttons and add structure for sing up

--- frontend/src/app/features/sing-up/sing-up.component.ts
+++ frontend/src/app/features/sing-up/sing-up.component.ts
@@ -1,12 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '@/shared/ui/card/card.component';
+import { AuthTypeComponent } from '@/shared/ui/auth-type/auth-type.component';
+import { InputComponent } from '@/shared/ui/input/input.component';
+import { ButtonComponent } from '@/shared/ui/button/button.component';
+import { UserIcon } from '@/shared/ui/icons/user.icon';
+import { LockIcon } from '@/shared/ui/icons/lock.icon';
 
 @Component({
   selector: 'app-sing-up',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, AuthTypeComponent, InputComponent, ButtonComponent, UserIcon, LockIcon],
   templateUrl: './sing-up.component.html',
-  styleUrl: './sing-up.component.scss'
+  styleUrl: './sing-up.component.scss',
 })
 export class SingUpComponent {
-
+  formValues = signal({
+    email: {
+      value: '',
+      type: 'text',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    password: {
+      value: '',
+      type: 'password',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    name: {
+      value: '',
+      type: 'text',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    cpf: {
+      value: '',
+      type: 'text',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    phone: {
+      value: '',
+      type: 'text',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    cep: {
+      value: '',
+      type: 'text',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    street: {
+      value: '',
+      type: 'text',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    number: {
+      value: '',
+      type: 'number',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    complement: {
+      value: '',
+      type: 'text',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+  });
 }

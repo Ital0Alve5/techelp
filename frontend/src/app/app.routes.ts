589903bc6429c762c9f2f4c6f7adc77219f8a7f1
italo <Ital0Alve5>
2024-09-13 19:35:48 -0300

    de

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SingUpComponent } from './features/sing-up/sing-up.component';
 import { ClientPanelComponent } from './features/client-panel/client-panel.component';
 
 export const routes: Routes = [
+  
   {
     path: 'login',
     component: LoginComponent,

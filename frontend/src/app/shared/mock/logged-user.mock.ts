f3331f173c73974cce724d4ba0f215047b5e15bf
YasmimZedelinski-Atlas <yasmim.zedelinski@atlastechnol.com>
2024-09-14 13:15:12 -0300

    feat: criado mock de usuário registrado

---
+++ frontend/src/app/shared/mock/logged-user.mock.ts
@@ -0,0 +1,8 @@
+export const loggedUserMock = [
+  {
+    cpf: '12342514425',
+    name: 'ítalo Alves de Paula e Silva',
+    email: 'italo_alves_@outlook.com',
+    phone: '81999614613',
+  },
+];
---
+++ frontend/src/app/shared/mock/registered-users.mock.ts
@@ -0,0 +1,18 @@
+export const registeredUsersMock = [
+  {
+    cpf: '12342514425',
+    name: 'ítalo Alves de Paula e Silva',
+    email: 'italo_alves_@outlook.com',
+    phone: '81999614613',
+    password: '1234',
+    address: {
+      cep: '83320340',
+      neighborhood: 'Pineville',
+      city: 'Pinhais',
+      state: 'Paraná',
+      street: 'Rua Salgado Filho',
+      number: 2100,
+      complement: 'Condomínio Portal de Pinhais- bloco A3 apt 44',
+    },
+  },
+];

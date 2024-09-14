afb6a2a27ea4cc31abb456ddcd151c34c279478a
Yasmim Zedelinski <yasmim.zedelinski@gmail.com>
2024-09-14 14:03:22 -0300

    feat: adicionar mais dados

--- frontend/src/app/shared/mock/registered-users.mock.ts
+++ frontend/src/app/shared/mock/registered-users.mock.ts
@@ -1,7 +1,7 @@
 export const registeredUsersMock = [
   {
     cpf: '12342514425',
-    name: 'ítalo Alves de Paula e Silva',
+    name: 'Ítalo Alves de Paula e Silva',
     email: 'italo_alves_@outlook.com',
     phone: '81999614613',
     password: '1234',
@@ -13,6 +13,38 @@ export const registeredUsersMock = [
       street: 'Rua Salgado Filho',
       number: 2100,
       complement: 'Condomínio Portal de Pinhais- bloco A3 apt 44',
+    },
+  },
+  {
+    cpf: '11092891978',
+    name: 'Yasmim Zedelinski Belão Rodrigues',
+    email: 'yasmim.zedelinski@gmail.com',
+    phone: '41987599563',
+    password: '5678',
+    address: {
+      cep: '83320340',
+      neighborhood: 'Pineville',
+      city: 'Pinhais',
+      state: 'Paraná',
+      street: 'Rua Salgado Filho',
+      number: 2100,
+      complement: 'Condomínio Portal de Pinhais- bloco A3 apt 44',
+    },
+  },
+  {
+    cpf: '64666374507',
+    name: 'Daniele Terra da Cunha',
+    email: 'daniele.cunha@gmail.com',
+    phone: '77985875470',
+    password: '9012',
+    address: {
+      cep: '83320340',
+      neighborhood: 'Centro',
+      city: 'Curitiba',
+      state: 'Paraná',
+      street: 'Rua XV de Novembro',
+      number: 8910,
+      complement: 'Perto do prédio histórico da UFPR',
     },
   },
 ];

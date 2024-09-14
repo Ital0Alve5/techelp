6f286247d09d30774bc568a5c46625ab899150d9
Yasmim Zedelinski <yasmim.zedelinski@gmail.com>
2024-09-14 13:44:19 -0300

    feat: criado mock para solicitações

---
+++ frontend/src/app/shared/mock/request.mock.ts
@@ -0,0 +1,16 @@
+export const requestHistoryMock = {
+  id: 1,
+  equipmentDescription: 'Lorem Ipsum is simply dummy',
+  equipmentCategory: 'Notebook',
+  issueDescription: 'Tela azul ao ligar, windows é muito ruim',
+  price: '99,90',
+  history: [
+    {
+      date: '01/09/2024',
+      hour: '13:24',
+      fromStatus: 'Rejeitada',
+      toStatus: 'Aprovada',
+      employee: 'Fulano de Tal',
+    },
+  ],
+};

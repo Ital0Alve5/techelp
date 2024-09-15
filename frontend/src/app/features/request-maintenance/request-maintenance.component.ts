a7633f203332b6187b5a6c67f53d86487492a1e7
pedrojoao <joaoabreu@ufpr.br>
2024-09-15 20:25:55 -0300

    feat: começando a validação do forms de manutenção

--- frontend/src/app/features/request-maintenance/request-maintenance.component.ts
+++ frontend/src/app/features/request-maintenance/request-maintenance.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '@/shared/ui/card/card.component';
 import { AuthTypeComponent } from '@/shared/ui/auth-type/auth-type.component';
 import { TextareaComponent } from '@/shared/ui/textarea/textarea.component';
@@ -6,6 +6,8 @@ import { ButtonComponent } from '@/shared/ui/button/button.component';
 import { UserIcon } from '@/shared/ui/icons/user.icon';
 import { LockIcon } from '@/shared/ui/icons/lock.icon';
 import { SelectComponent } from '@/shared/ui/select/select.component';
+import { ValidatorService } from '@/shared/services/input/validator.service';
+import { Validation } from '@/shared/enums/validation.enum';
 
 @Component({
   selector: 'app-request-maintenance',
@@ -14,4 +16,125 @@ import { SelectComponent } from '@/shared/ui/select/select.component';
   templateUrl: './request-maintenance.component.html',
   styleUrls: ['./request-maintenance.component.scss'],
 })
-export class RequestMaintenanceComponent {}
+export class RequestMaintenanceComponent {
+  formValues = signal({
+    deviceCategory: {
+      value: '',
+      type: 'select',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    deviceDescription: {
+      value: '',
+      type: 'text',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+    defectDescription: {
+      value: '',
+      type: 'text',
+      validation: {
+        error: false,
+        message: '',
+      },
+    },
+  });
+
+  constructor(private validatorService: ValidatorService) {}
+
+  onInputChange(fieldName: string, newValue: string) {
+    this.formValues.update((currentValues) => ({
+      ...currentValues,
+      [fieldName]: {
+        value: newValue,
+      },
+    }));
+  }
+
+  resetInputs() {
+    this.formValues.update((currentValues) => ({
+      ...currentValues,
+      deviceCategory: {
+        value: '',
+        type: 'select',
+        validation: {
+          error: false,
+          message: '',
+        },
+      },
+      deviceDescription: {
+        value: '',
+        type: 'text',
+        validation: {
+          error: false,
+          message: '',
+        },
+      },
+      defectDescription: {
+        value: '',
+        type: 'text',
+        validation: {
+          error: false,
+          message: '',
+        },
+      },
+    }));
+  }
+
+  sendData() {
+    console.log({
+      deviceDescription: this.formValues().deviceDescription.value,
+      deviceCategory: this.formValues().deviceCategory.value,
+      defectDescription: this.formValues().defectDescription.value,
+    });
+  }
+
+  onSubmit() {
+
+    const deviceMinLengthValidation = this.validatorService.setValidation(
+      this.formValues().deviceDescription.value, 
+      Validation.MinLength, 
+      { minLength: 5 }
+    );
+
+    const deviceMaxLengthValidation = this.validatorService.setValidation(
+      this.formValues().deviceDescription.value, 
+      Validation.MaxLength, 
+      { maxLength: 50 }
+    );
+
+    const deviceDescriptionValidation = {
+      error: deviceMinLengthValidation.error || deviceMaxLengthValidation.error,
+      message: deviceMinLengthValidation.error ? deviceMinLengthValidation.message : deviceMaxLengthValidation.message,
+    };
+
+    const defectMinLengthValidation = this.validatorService.setValidation(
+      this.formValues().defectDescription.value, 
+      Validation.MinLength, 
+      { minLength: 5 }
+    );
+  
+    const defectMaxLengthValidation = this.validatorService.setValidation(
+      this.formValues().defectDescription.value, 
+      Validation.MaxLength, 
+      { maxLength: 150 }
+    );
+  
+    const defectDescriptionValidation = {
+      error: defectMinLengthValidation.error || defectMaxLengthValidation.error,
+      message: defectMinLengthValidation.error ? defectMinLengthValidation.message : defectMaxLengthValidation.message,
+    };
+  
+    this.formValues().deviceDescription.validation = deviceDescriptionValidation;
+    this.formValues().defectDescription.validation = defectDescriptionValidation;
+  
+    if (!deviceDescriptionValidation.error && !defectDescriptionValidation.error) {
+      this.sendData();
+      this.resetInputs();
+    }
+  }  
+}

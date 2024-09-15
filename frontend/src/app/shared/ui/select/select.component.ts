44d7cedc032e3f0bec8919d1861b5af883818747
Juliano Filho <julianofilho@ufpr.br>
2024-09-15 02:27:32 -0300

    feature: retirada da sintaxe annotations e adicao das categorias do select

--- frontend/src/app/shared/ui/select/select.component.ts
+++ frontend/src/app/shared/ui/select/select.component.ts
@@ -1,20 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-select',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
 })
 export class SelectComponent {
-  @Input() id: string = '';
-  @Input() label: string = '';
-  @Input() placeholder: string = '';
-  @Input() options: { value: string, label: string }[] = [];
-  @Input() value: string = '';
+  id = input<string>(''); // input() substitui o @Input()
+  label = input<string>(''); // input() substitui o @Input()
+  placeholder = input<string>(''); // input() substitui o @Input()
+  options = input<{ value: string, label: string }[]>([]); // Usando input() para a lista de opções
+  value = signal<string>(''); // signal() para manter o estado do valor selecionado
 
   onChange(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
-    this.value = selectElement.value;
+    this.value.set(selectElement.value); // Atualiza o signal com o valor selecionado
   }
 }

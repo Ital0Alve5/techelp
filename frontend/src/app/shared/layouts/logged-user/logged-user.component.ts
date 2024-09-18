e0bdc0a1bc4ac7ab721dc969662800b28df320da
Yasmim Zedelinski <yasmim.zedelinski@gmail.com>
2024-09-18 15:27:50 -0300

    feat: cria novo componente de layout para usuários logados

---
+++ frontend/src/app/shared/layouts/logged-user/logged-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logged-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './logged-user.component.html',
+  styleUrl: './logged-user.component.scss'
+})
+export class LoggedUserComponent {
+
+}

afd14623c37c36c3e9dfc20260e02f9a8e6d0771
pedrojoao <joaoabreu@ufpr.br>
2024-09-23 11:36:04 -0300

    feat: insercao de modal de Aprovar

--- frontend/src/app/features/budget/budget.component.ts
+++ frontend/src/app/features/budget/budget.component.ts
@@ -4,14 +4,17 @@ import { CardComponent } from '@/shared/ui/card/card.component';
 import { ArrowRightIcon } from '@/shared/ui/icons/arrow-right.icon';
 import { RouterLink } from '@angular/router';
 import { maintenanceRequests } from '@/shared/mock/maintenance-requests.mock';
+import { ModalComponent } from '@/shared/ui/modal/modal.component';
+
 @Component({
   selector: 'app-budget',
   standalone: true,
-  imports: [ButtonComponent, CardComponent, ArrowRightIcon, RouterLink],
+  imports: [ButtonComponent, CardComponent, ArrowRightIcon, RouterLink, ModalComponent],
   templateUrl: './budget.component.html',
   styleUrl: './budget.component.scss',
 })
 export class BudgetComponent {
+  isApprovalModalOpen = false;
   userId: number = JSON.parse(localStorage.getItem('userId')!);
   requestId: number = Number.parseInt(window.location.pathname.match(/\/orcamento\/(\d+)/)![1]);
   requestData = {
@@ -23,6 +26,14 @@ export class BudgetComponent {
     hour: '',
     employee: '',
   };
+
+  openModal() {
+    this.isApprovalModalOpen = true;
+  }
+
+  closeModal() {
+    this.isApprovalModalOpen = false;
+  }
 
   constructor() {
     maintenanceRequests.forEach((request) => {

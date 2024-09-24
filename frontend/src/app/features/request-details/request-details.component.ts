132c23d1520d24492c7b762375b0cd3b8ea58d8f
Yasmim Zedelinski <yasmim.zedelinski@gmail.com>
2024-09-23 22:21:58 -0300

    feat: cria novo componente para a tela do botão 'visualizar'

---
+++ frontend/src/app/features/request-details/request-details.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '@/shared/ui/button/button.component';
+import { CardComponent } from '@/shared/ui/card/card.component';
+import { ArrowRightIcon } from '@/shared/ui/icons/arrow-right.icon';
+import { RouterLink } from '@angular/router';
+import { maintenanceRequests } from '@/shared/mock/maintenance-requests.mock';
+
+@Component({
+  selector: 'app-request-details',
+  standalone: true,
+  imports: [ButtonComponent, CardComponent, ArrowRightIcon, RouterLink],
+  templateUrl: './request-details.component.html',
+  styleUrl: './request-details.component.scss',
+})
+export class RequestDetailsComponent {
+  userId: number = JSON.parse(localStorage.getItem('userId')!);
+  requestId: number = Number.parseInt(window.location.pathname.match(/\/orcamento\/(\d+)/)![1]);
+  requestData = {
+    deviceDescription: '',
+    deviceCategory: '',
+    issueDescription: '',
+    price: '',
+    date: '',
+    hour: '',
+    employee: '',
+  };
+
+  constructor() {
+    maintenanceRequests.forEach((request) => {
+      if (request.userId === this.userId && this.requestId === request.id) {
+        this.requestData = {
+          deviceDescription: request.deviceDescription,
+          deviceCategory: request.deviceCategory,
+          issueDescription: request.issueDescription,
+          price: request.price,
+          date: request.date,
+          hour: request.hour,
+          employee: request.history[request.history.length - 1].employee,
+        };
+      }
+    });
+  }
+}

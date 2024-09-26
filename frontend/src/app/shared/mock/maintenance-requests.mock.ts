b22ffc3027ebbbfaae996ce727ff06aa4c031e35
PaG0Dezaum <heitor.hes@hotmail.com>
2024-09-26 10:03:54 -0300

    adiciona mais request mocks

--- frontend/src/app/shared/mock/maintenance-requests.mock.ts
+++ frontend/src/app/shared/mock/maintenance-requests.mock.ts
@@ -2,7 +2,7 @@ export const maintenanceRequests = [
   {
     id: 1,
     userId: 1,
-    deviceDescription: 'Lorem Ipsum is simply dummy',
+    deviceDescription: 'M-C donalds',
     deviceCategory: 'Notebook',
     issueDescription: 'Tela azul ao ligar, windows é muito ruim',
     price: '99,90',
@@ -45,7 +45,7 @@ export const maintenanceRequests = [
   {
     id: 2,
     userId: 1,
-    deviceDescription: 'hry hey hey hey',
+    deviceDescription: 'A-hey hey hey hey',
     deviceCategory: 'Notebook',
     issueDescription: 'Quero botar linux',
     price: '99,90',
@@ -84,6 +84,153 @@ export const maintenanceRequests = [
         employee: 'Maria Oliveira',
       },
     ],
+  },
+  {
+    id: 5,
+    userId: 1,
+    deviceDescription: 'R io de janeiroei laa abla bal',
+    deviceCategory: 'Notebook',
+    issueDescription: 'Tela azul ao ligar, windows é muito ruim',
+    price: '191119,90',
+    date: '01/09/2024',
+    hour: '13:24',
+    currentStatus: 'Aberta',
+    rejectReason: '',
+    history: [
+      {
+        date: '01/09/2024',
+        hour: '13:24',
+        fromStatus: 'Rejeitada',
+        toStatus: 'Aberta',
+        employee: 'bw',
+      },
+    ]
+  },
+  {
+    id: 6,
+    userId: 1,
+    deviceDescription: 'Indio',
+    deviceCategory: 'Notebook',
+    issueDescription: ' akjasd daks dasjdjka ',
+    price: '99,90',
+    date: '01/09/2024',
+    hour: '13:24',
+    currentStatus: 'Aguardando Pagamento',
+    rejectReason: '',
+    history: [
+      {
+        date: '01/09/2024',
+        hour: '13:24',
+        fromStatus: 'Rejeitada',
+        toStatus: 'Aguardando Pagamento',
+        employee: 'Fulano de Tal',
+      },
+    ]
+  },
+  {
+    id: 7,
+    userId: 1,
+    deviceDescription: 'AAAAAAAAAA',
+    deviceCategory: 'Celular',
+    issueDescription: 'AKNKNAdkndnk',
+    price: '99,90',
+    date: '01/09/2024',
+    hour: '13:24',
+    currentStatus: 'Paga',
+    rejectReason: '',
+    history: [
+      {
+        date: '01/09/2024',
+        hour: '13:24',
+        fromStatus: 'Rejeitada',
+        toStatus: 'Paga',
+        employee: 'DUlcio',
+      },
+    ]
+  },
+  {
+    id: 8,
+    userId: 1,
+    deviceDescription: 'Joao estragou o pc',
+    deviceCategory: 'Notebook',
+    issueDescription: 'Tela azul ao ligar, windows é muito ruim',
+    price: '99,90',
+    date: '01/09/2024',
+    hour: '13:24',
+    currentStatus: 'Aprovada',
+    rejectReason: '',
+    history: [
+      {
+        date: '01/09/2024',
+        hour: '13:24',
+        fromStatus: 'Rejeitada',
+        toStatus: 'Aprovada',
+        employee: 'Fulano de Tal',
+      },
+    ]
+  },
+  {
+    id: 9,
+    userId: 1,
+    deviceDescription: 'Uiiii',
+    deviceCategory: 'Notebook',
+    issueDescription: 'Tela azul ao ligar, windows é muito ruim',
+    price: '99,90',
+    date: '01/09/2024',
+    hour: '13:24',
+    currentStatus: 'Rejeitada',
+    rejectReason: '',
+    history: [
+      {
+        date: '01/09/2024',
+        hour: '13:24',
+        fromStatus: 'Arrumada',
+        toStatus: 'Rejeitada',
+        employee: 'Fulano de Tal',
+      },
+    ]
+  },
+  {
+    id: 10,
+    userId: 1,
+    deviceDescription: 'Lorem Ipsum is simply dummy',
+    deviceCategory: 'Notebook',
+    issueDescription: 'Tela azul ao ligar, windows é muito ruim',
+    price: '99,90',
+    date: '01/09/2024',
+    hour: '13:24',
+    currentStatus: 'Finalizada',
+    rejectReason: '',
+    history: [
+      {
+        date: '01/09/2024',
+        hour: '13:24',
+        fromStatus: 'Rejeitada',
+        toStatus: 'Finalizada',
+        employee: 'Fulano de Tal',
+      },
+    ]
+  },
+  {
+    id: 11,
+    userId: 1,
+    deviceDescription: 'Interesting topics fomaasasasgit',
+    deviceCategory: 'Notebook',
+    issueDescription: 'Tela azul ao ligar, windows é muito ruim',
+    price: '99,90',
+    date: '01/09/2024',
+    hour: '13:24',
+    currentStatus: 'Outros',
+    rejectReason: '',
+    history: [
+      {
+        date: '01/09/2024',
+        hour: '13:24',
+        fromStatus: 'Finalizada',
+        toStatus: 'Outros',
+        employee: 'Fulano de Tal',
+      },
+    ]
   },
   {
     id: 3,

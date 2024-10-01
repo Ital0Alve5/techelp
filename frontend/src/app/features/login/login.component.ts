cc83da8dc4af391ca61a1bfe9abd369a2cad2a47
italo <Ital0Alve5>
2024-10-01 20:21:33 -0300

    feat: create initial employee page

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -69,7 +69,7 @@ export class LoginComponent {
           });
         } else if (this.employeeLogin) {
           this.authenticator.authenticate(true);
-          this.router.navigate([`/`]);
+          this.router.navigate([`/funcionario/${(response.data as { userId: number }).userId}/solicitacoes/abertas`]);
         } else {
           this.authenticator.authenticate(true);
           this.router.navigate([`/cliente/${(response.data as { userId: number }).userId}/solicitacoes`]);

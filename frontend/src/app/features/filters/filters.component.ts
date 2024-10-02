d46af77fe01b162d373efce796af88638f1235b4
Yasmim Zedelinski <yasmim.zedelinski@gmail.com>
2024-10-02 17:27:02 -0300

    feat: adiciona novo componente de data e estilos do filtro

--- frontend/src/app/features/filters/filters.component.ts
+++ frontend/src/app/features/filters/filters.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { ButtonComponent } from '@/shared/ui/button/button.component';
+
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.scss',
 })

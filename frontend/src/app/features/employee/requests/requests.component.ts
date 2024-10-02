64756a1585c22bc94c38d18ea65b402092d8d951
Yasmim Zedelinski <yasmim.zedelinski@gmail.com>
2024-10-02 17:00:08 -0300

    feat: adiciona novo componenre

--- frontend/src/app/features/employee/requests/requests.component.ts
+++ frontend/src/app/features/employee/requests/requests.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+
 import { EmployeeTableRowComponent } from '../components/components/client-table-row/employee-table-row.component';
 import { ButtonComponent } from '@/shared/ui/button/button.component';
 import { LinkAsButtonComponent } from '@/shared/ui/link-as-button/link-as-button.component';
 import { Requests } from '@/shared/types/api/maintenance-requests.type';
 import { RequestsService } from '@/shared/services/requests/requests.service';
+import { FiltersComponent } from '@/features/filters/filters.component';
 
 @Component({
   selector: 'app-requests',
   standalone: true,
   providers: [RequestsService],
-  imports: [EmployeeTableRowComponent, ButtonComponent, LinkAsButtonComponent],
+  imports: [EmployeeTableRowComponent, ButtonComponent, LinkAsButtonComponent, FiltersComponent],
   templateUrl: './requests.component.html',
   styleUrl: './requests.component.scss',
 })
---
+++ frontend/src/app/features/filters/filters.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  standalone: true,
+  imports: [],
+  templateUrl: './filters.component.html',
+  styleUrl: './filters.component.scss',
+})
+export class FiltersComponent {
+  constructor() {
+    console.log('oi');
+  }
+}

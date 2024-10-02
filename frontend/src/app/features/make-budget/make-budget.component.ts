bc77375d226932cfac9928c0e1166b9eff6eb2e9
PaG0Dezaum <heitor.hes@hotmail.com>
2024-10-02 20:57:51 -0300

    ajeitei o type das solicitacoes e condicionei as variaveis no html

--- frontend/src/app/features/make-budget/make-budget.component.ts
+++ frontend/src/app/features/make-budget/make-budget.component.ts
@@ -1,59 +1,73 @@
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RequestsService } from '@/shared/services/requests/requests.service';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+import { Requests } from '@/shared/types/api/maintenance-requests.type';
 
 @Component({
-    selector: 'app-make-budget',
-    imports: [FormsModule, CommonModule],
-    standalone: true,
-    templateUrl: './make-budget.component.html',
-    styleUrls: ['./make-budget.component.scss'],
-    providers: [RequestsService],
-  })
-  export class MakeBudgetComponent {
-    @Input() solicitacao: any = {}; 
-    showModal: boolean = false;
-    valorOrcamento: number = 0;
-    submitted: boolean = false;
-    funcionarioLogado = { nome: 'Funcionário Exemplo' };
-  
-    constructor(private requestsService: RequestsService) {}
-  
-    openModal() {
-      if (this.solicitacao?.currentStatus === 'ABERTO') {
-        this.showModal = true;
-        this.submitted = false;
-      }
+  selector: 'app-make-budget',
+  imports: [FormsModule, CommonModule],
+  standalone: true,
+  templateUrl: './make-budget.component.html',
+  styleUrls: ['./make-budget.component.scss'],
+  providers: [RequestsService],
+})
+export class MakeBudgetComponent {
+  @Input() solicitacao: Requests = {
+    id: 0,
+    userId: 0,
+    employeeId: 0,
+    deviceDescription: '',
+    deviceCategory: '',
+    issueDescription: '',
+    price: 'd',
+    date: '',
+    hour: '',
+    currentStatus: '',
+    rejectReason: '',
+    history: [],
+  };
+  showModal: boolean = false;
+  valorOrcamento: number = 0;
+  submitted: boolean = false;
+  funcionarioLogado = { nome: 'Funcionário Exemplo' };
+
+  constructor(private requestsService: RequestsService) {}
+
+  openModal() {
+    if (this.solicitacao?.currentStatus === 'ABERTO') {
+      this.showModal = true;
+      this.submitted = false;
     }
-  
-    cancelar() {
+  }
+
+  cancelar() {
+    this.showModal = false;
+  }
+
+  confirmarOrcamento() {
+    if (this.valorOrcamento) {
+      const orcamento = {
+        valor: this.valorOrcamento,
+        funcionario: this.funcionarioLogado.nome,
+        data: new Date(),
+      };
+
+      this.solicitacao.currentStatus = 'ORÇADA';
+      this.solicitacao.price = orcamento.valor.toString();
+
+      console.log('Orçamento registrado:', this.solicitacao);
+
+      this.submitted = true;
       this.showModal = false;
     }
-  
-    confirmarOrcamento() {
-      if (this.valorOrcamento) {
-        const orcamento = {
-          valor: this.valorOrcamento,
-          funcionario: this.funcionarioLogado.nome,
-          data: new Date(),
-        };
-  
-        this.solicitacao.currentStatus = 'ORÇADA';
-        this.solicitacao.orcamento = orcamento;
-  
-        console.log('Orçamento registrado:', this.solicitacao);
-  
-        this.submitted = true;
-        this.showModal = false;
-      }
-    }
-  
-    isSubmitted() {
-      return this.submitted;
-    }
-  
-    isModalVisible() {
-      return this.showModal;
-    }
-  }  
+  }
+
+  isSubmitted() {
+    return this.submitted;
+  }
+
+  isModalVisible() {
+    return this.showModal;
+  }
+}

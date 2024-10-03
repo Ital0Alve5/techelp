5f5e0c9b1b6b812dfc699b13efabeac2558e95d3
Yasmim Zedelinski <yasmim.zedelinski@gmail.com>
2024-10-03 10:09:39 -0300

    feat: inicia lógica dos filtros

--- frontend/src/app/features/filters/components/date/date.component.ts
+++ frontend/src/app/features/filters/components/date/date.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-date',
   standalone: true,
   imports: [],
   templateUrl: './date.component.html',
-  styleUrl: './date.component.scss'
+  styleUrl: './date.component.scss',
 })
 export class DateComponent {
+  startDate: string | null = null;
+  endDate: string | null = null;
 
+  @Output() dateRangeSelected = new EventEmitter<{ startDate: string | null; endDate: string | null }>();
+
+  onDateChange() {
+    this.dateRangeSelected.emit({ startDate: this.startDate, endDate: this.endDate });
+  }
 }
--- frontend/src/app/features/filters/filters.component.ts
+++ frontend/src/app/features/filters/filters.component.ts
@@ -1,16 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ButtonComponent } from '@/shared/ui/button/button.component';
+import { DateComponent } from './components/date/date.component';
+import { RequestsService } from '@/shared/services/requests/requests.service';
+import { Requests } from '@/shared/types/api/maintenance-requests.type';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, DateComponent],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.scss',
 })
-export class FiltersComponent {
-  constructor() {
-    console.log('oi');
+export class FiltersComponent implements OnInit {
+  requests: Requests[] = [];
+  filteredRequests: Requests[] = [];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private requestsService: RequestsService,
+  ) {}
+
+  ngOnInit(): void {
+    this.requests = this.requestsService.getRequest();
+
+    this.route.queryParams.subscribe((params) => {
+      const filter = params['filter'];
+
+      if (filter === 'today') {
+        this.filterToday();
+      } else {
+        this.filterAll();
+      }
+    });
+  }
+
+  filterBy(filter: string) {
+    const queryParams = { filter };
+
+    if (filter === 'today') {
+      this.filterToday();
+    } else if (filter === 'all') {
+      this.filterAll();
+    }
+
+    this.router.navigate([], { queryParams });
+  }
+
+  filterToday() {
+    const today = new Date().toISOString().split('T')[0];
+    this.filteredRequests = this.requests.filter((req) => req.date === today);
+  }
+
+  filterAll() {
+    this.filteredRequests = this.requests;
   }
 }

54ccae9e7504d960cf69767d82f3a422f6a6bdf4
Juliano Filho <julianofilho@ufpr.br>
2024-10-03 01:14:53 -0300

    feat: component do orçamento ainda sem funcionar

--- frontend/src/app/features/make-budget/make-budget.component.ts
+++ frontend/src/app/features/make-budget/make-budget.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RequestsService } from '@/shared/services/requests/requests.service';
 import { CommonModule } from '@angular/common';
@@ -12,24 +12,52 @@ import { Requests } from '@/shared/types/api/maintenance-requests.type';
   styleUrls: ['./make-budget.component.scss'],
   providers: [RequestsService],
 })
-export class MakeBudgetComponent {
-  @Input() solicitacao!: Requests;
+export class MakeBudgetComponent implements OnInit {
+  solicitacao!: Requests;
+  requestId!: number;
   showModal: boolean = false;
   valorOrcamento: number = 0;
   submitted: boolean = false;
-  funcionarioLogado!: { nome: string };
+  funcionarioLogado = { nome: 'Funcionário Exemplo' };
+  notFoundMessage: string = '';
+  successMessage: string = '';
 
-  constructor(private requestsService: RequestsService) {}
+  constructor(private requestsService: RequestsService) {
+    this.requestId = this.getRequestIdFromUrl();
+  }
+
+  ngOnInit() {
+    this.searchRequest();
+  }
+
+  getRequestIdFromUrl(): number {
+    const pathSegments = location.pathname.split('/');
+    return parseInt(pathSegments[pathSegments.length - 1].trim());
+  }
+
+  searchRequest() {
+    const maintenanceRequests = this.requestsService.getRequest('Aberta');
+    this.solicitacao = maintenanceRequests.find((request) => request.id === this.requestId)!;
+
+    if (!this.solicitacao) {
+      this.notFoundMessage = 'Solicitação não encontrada';
+    } else {
+      this.notFoundMessage = '';
+    }
+  }
 
   openModal() {
     if (this.solicitacao?.currentStatus === 'ABERTO') {
       this.showModal = true;
       this.submitted = false;
+      this.successMessage = '';
     }
   }
 
   cancelar() {
     this.showModal = false;
+    this.submitted = false;
+    this.successMessage = '';
   }
 
   confirmarOrcamento() {
@@ -42,11 +70,20 @@ export class MakeBudgetComponent {
 
       this.solicitacao.currentStatus = 'ORÇADA';
       this.solicitacao.price = orcamento.valor.toString();
-
       console.log('Orçamento registrado:', this.solicitacao);
-
+      this.successMessage = 'Orçamento registrado com sucesso!';
       this.submitted = true;
       this.showModal = false;
+    } else {
+      console.log('Valor de orçamento inválido');
     }
+  }
+
+  isSubmitted() {
+    return this.submitted;
+  }
+
+  isModalVisible() {
+    return this.showModal;
   }
 }

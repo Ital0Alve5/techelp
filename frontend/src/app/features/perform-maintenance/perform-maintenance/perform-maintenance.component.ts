b6496a0d9df6a87aae71d3b9cc86a9437694f507
PaG0Dezaum <heitor.hes@hotmail.com>
2024-10-05 21:14:40 -0300

    Adiciona caixas de informação da solicitação e cliente

--- frontend/src/app/features/perform-maintenance/perform-maintenance/perform-maintenance.component.ts
+++ frontend/src/app/features/perform-maintenance/perform-maintenance/perform-maintenance.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import { maintenanceRequests } from '@/shared/mock/maintenance-requests.mock';
+import { registeredUsersMock } from '@/shared/mock/registered-users.mock';
 
 @Component({
   selector: 'app-perform-maintenance',
   standalone: true,
   imports: [],
   templateUrl: './perform-maintenance.component.html',
-  styleUrl: './perform-maintenance.component.scss'
+  styleUrl: './perform-maintenance.component.scss',
 })
 export class PerformMaintenanceComponent {
+  employeeId: number = JSON.parse(localStorage.getItem('userId')!);
+  requestId: number = Number.parseInt(window.location.pathname.match(/\/manutencao\/(\d+)/)![1]);
 
+  requestData = {
+    deviceDescription: '',
+    deviceCategory: '',
+    issueDescription: '',
+    price: '',
+    date: '',
+    hour: '',
+    employee: '',
+    currentStatus: '',
+    userId: -1,
+  };
+
+  clientData = {
+    name: '',
+    email: '',
+    phone: '',
+  };
+
+  constructor() {
+    maintenanceRequests.forEach((request) => {
+      if (request.employeeId === this.employeeId && this.requestId === request.id) {
+        this.requestData = {
+          deviceDescription: request.deviceDescription,
+          deviceCategory: request.deviceCategory,
+          issueDescription: request.issueDescription,
+          price: request.price,
+          date: request.date,
+          hour: request.hour,
+          employee: request.history[request.history.length - 1].employee,
+          currentStatus: request.currentStatus,
+          userId: request.userId,
+        };
+      }
+    });
+
+    registeredUsersMock.forEach((client) => {
+      if (client.id == this.requestData.userId) {
+        this.clientData = {
+          name: client.name,
+          email: client.email,
+          phone: `(${client.phone.substring(0, 2)})${client.phone.substring(2, 6)}-${client.phone.substring(6)}`,
+        };
+      }
+    });
+  }
 }

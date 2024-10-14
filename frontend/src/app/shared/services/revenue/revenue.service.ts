5aba4ce7679da46246a78b26765898c7447cdba4
PaG0Dezaum <heitor.hes@hotmail.com>
2024-10-14 11:23:34 -0300

    service now returns correct object with date filters

--- frontend/src/app/shared/services/revenue/revenue.service.ts
+++ frontend/src/app/shared/services/revenue/revenue.service.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@angular/core';
+import { maintenanceRequests } from '@/shared/mock/maintenance-requests.mock';
+import { Requests } from '@/shared/types/api/maintenance-requests.type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RevenueService {
-
   //Parametros: passar datas no formato "YYYY-MM-DD".
   //Retorno: retorna array de objetos com todas as receitas entre as datas fornecidas.
   getRevenues(startDate: string = '2024-01-01', finalDate: string = new Date().toISOString().split('T')[0]) {
-    console.log(startDate + " --- " + finalDate);
+    let requests: Requests[] = [...maintenanceRequests];
+    requests = requests.filter(({ currentStatus }) => currentStatus === 'Paga');
+
+    const startDateComparison = startDate.split('-').join('');
+    const finalDateComparison = finalDate.split('-').join('');
+
+    requests = requests.filter(({ date }) => {
+      const dateComparison = this.reshapeDate(date);
+      return dateComparison >= startDateComparison && dateComparison <= finalDateComparison;
+    });
+
+    const result = requests.map(({ date, price, deviceCategory }) => ({
+      date,
+      price,
+      deviceCategory
+    }));
+
+    console.log(result);
+  }
+
+  private reshapeDate(date: string): string {
+    const [day, month, year] = date.split('/');
+    return `${year}${month}${day}`;
   }
 }

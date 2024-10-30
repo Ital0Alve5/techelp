3d81c1820891c37c6a4de2168b89a8ba676640e1
italo <Ital0Alve5>
2024-10-29 21:37:52 -0300

    feat: add validations

--- backend/api/src/main/java/com/techelp/api/controller/SignUpController.java
+++ backend/api/src/main/java/com/techelp/api/controller/SignUpController.java
@@ -1,6 +1,7 @@
 package com.techelp.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.techelp.api.model.ClientModel;
 import com.techelp.api.service.SignUpService;
 
+import jakarta.validation.Valid;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class SignUpController {
@@ -18,8 +21,8 @@ public class SignUpController {
 	private SignUpService signUpService;
 
 	@PostMapping("/signUp")
-	public ClientModel addNewClient(@RequestBody ClientModel client) {
-		return this.signUpService.addClient(client);
+	public ResponseEntity<ClientModel> addNewClient(@RequestBody @Valid ClientModel client) {
+		return ResponseEntity.ok(this.signUpService.addClient(client));
 	}
 
 	@GetMapping("/")

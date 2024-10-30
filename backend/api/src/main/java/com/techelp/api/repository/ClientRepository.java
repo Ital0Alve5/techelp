873784ccad7506bedd36d842c417f4b2091754e9
Lara Ono <lara.reis@atlastechnol.com>
2024-10-29 22:30:18 -0300

    feat: login backend

--- backend/api/src/main/java/com/techelp/api/repository/ClientRepository.java
+++ backend/api/src/main/java/com/techelp/api/repository/ClientRepository.java
@@ -1,9 +1,15 @@
 package com.techelp.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.techelp.api.model.ClientModel;
 
+import java.util.List;
+
 @Repository
-public interface ClientRepository extends JpaRepository<ClientModel, Integer> {}
+public interface ClientRepository extends JpaRepository<ClientModel, Integer> {
+
+    List<ClientModel> findByEmailAndPassword(String email, String password);
+}

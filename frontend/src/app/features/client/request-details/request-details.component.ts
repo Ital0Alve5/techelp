21e5ee00f38b85903f6e905de1414b9ede9fb2c6
Lara Ono <lara.reis@atlastechnol.com>
2024-11-06 01:08:47 -0300

    feat: implementacao do back de detalhes da solicitacao

--- frontend/src/app/features/client/request-details/request-details.component.ts
+++ frontend/src/app/features/client/request-details/request-details.component.ts
@@ -6,6 +6,10 @@ import { CardComponent } from '@/shared/ui/card/card.component';
 import { ArrowRightIcon } from '@/shared/ui/icons/arrow-right.icon';
 
 import { RequestDetailsService } from './services/request-details.service';
+import { PopupService } from '@/shared/services/pop-up/pop-up.service';
+import { Status } from '@/shared/ui/pop-up/enum/status.enum';
+import { ClientRequests } from '../requests-table/types/client-requests.type';
+import { ClientRequestHistory } from '../requests-table/types/client-request-history.type';
 
 @Component({
   selector: 'app-request-details',
@@ -19,10 +23,77 @@ export class RequestDetailsComponent {
   userId: number = JSON.parse(localStorage.getItem('userId')!);
   requestId: number = Number.parseInt(window.location.pathname.match(/\/solicitacao\/(\d+)/)![1]);
 
-  requestData = signal(this.requestDetailsService.getRequestDetailsByRequestId(this.requestId));
+  requestData = signal<ClientRequests>({
+    clientId: 0,
+    categoryId: 0,
+    deviceDescription: '',
+    issueDescription: '',
+    budget: 0,
+    orientation: null,
+    rejectReason: null,
+    status: '',
+    lastEmployee: null,
+    date: '',
+    hour: ''
+  });
+
+  dataHistory = signal<ClientRequestHistory[]>([]);
 
   constructor(
     public router: Router,
     private requestDetailsService: RequestDetailsService,
-  ) {}
+    private popupService : PopupService
+  ) {
+    this.getRequestDetailsByRequestId(this.requestId);
+    this.getRequestHistory(this.requestId);
+  }
+  async getRequestHistory(id: number) {
+    const success = await this.requestDetailsService.getRequestHistory(id);
+    if (!success?.data) {
+      this.popupService.addNewPopUp({
+        type: Status.Error,
+        message: 'Algo deu errado!',
+      });
+      return;
+    }
+
+    if ('errors' in success.data) {
+      Object.values(success.data.errors).forEach((error) => {
+        this.popupService.addNewPopUp({
+          type: Status.Error,
+          message: error,
+        });
+      });
+      return;
+    }
+    const requestHistory = success.data.data as unknown;
+    this.dataHistory.set(requestHistory as ClientRequestHistory[]);
+  }
+  async getRequestDetailsByRequestId(id: number) {
+
+    const success = await this.requestDetailsService.getRequestDetailsByRequestId(id);
+    if (!success?.data) {
+      this.popupService.addNewPopUp({
+        type: Status.Error,
+        message: 'Algo deu errado!',
+      });
+      return;
+    }
+
+    if ('errors' in success.data) {
+      Object.values(success.data.errors).forEach((error) => {
+        this.popupService.addNewPopUp({
+          type: Status.Error,
+          message: error,
+        });
+      });
+      return;
+    }
+    const maintenanceRequestDetails = success.data.data as unknown;
+    this.requestData.set(maintenanceRequestDetails as ClientRequests);
+
+  }
+
+  
 }
+

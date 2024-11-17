ecb8ddea85f00c89fa1dab1f2eee841ce29db2f2
pedrojoao <joaoabreu@ufpr.br>
2024-11-17 13:03:35 -0300

    Agora sim tudo funcionando

--- frontend/src/app/features/employee/categories/categories.component.ts
+++ frontend/src/app/features/employee/categories/categories.component.ts
@@ -1,17 +1,21 @@
-import { Component, signal, OnInit } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Categorie } from '@/shared/types/categorie.type';
 import { ButtonComponent } from '@/shared/ui/button/button.component';
 import { ModalComponent } from '@/shared/ui/modal/modal.component';
 import { FormsModule } from '@angular/forms';
+import { CategoriesService } from './service/categories.service';
 import { RouterLink } from '@angular/router';
 import { ArrowRightIcon } from '@/shared/ui/icons/arrow-right.icon';
 import { TableComponent } from '@/shared/ui/table/table.component';
 import { EditIcon } from '@/shared/ui/icons/edit/edit.icon';
 import { DeleteIcon } from '@/shared/ui/icons/delete/delete.icon';
 import { AddIcon } from '@/shared/ui/icons/add/add.icon';
+import { TextareaComponent } from '@/shared/ui/textarea/textarea.component';
+import { categorieData } from './model/categorie-data.model';
 import { RequiredValidator } from '@/shared/services/validators/required-validator.service';
 import { MinLengthValidator } from '@/shared/services/validators/min-length-validator.service';
-import { CategoriesService } from './service/categories.service';
+import { PopupService } from '@/shared/services/pop-up/pop-up.service';
+import { Status } from '@/shared/ui/pop-up/enum/status.enum';
 
 @Component({
   selector: 'app-categories',
@@ -26,16 +30,17 @@ import { CategoriesService } from './service/categories.service';
     DeleteIcon,
     AddIcon,
     TableComponent,
-  ],  
-  providers: [RequiredValidator, MinLengthValidator],
+    TextareaComponent,
+  ],
+  providers: [RequiredValidator, MinLengthValidator, CategoriesService],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss',
+  styleUrls: ['./categories.component.scss'],
 })
-export class CategoriesComponent implements OnInit {
+export class CategoriesComponent {
   userId: number = JSON.parse(localStorage.getItem('userId')!);
-  categories: Categorie[] = [];
-  newCategorieData = signal({ id: -1, value: '', validation: { error: false, message: '' } });
-  selectedCategoryData = signal({ id: -1, value: '', validation: { error: false, message: '' } });
+  categories = signal<Categorie[]>([]);
+  newCategorieData = signal(JSON.parse(JSON.stringify(categorieData)));
+  selectedCategoryData = signal(JSON.parse(JSON.stringify(categorieData)));
 
   isNewCategoryModalOpen = signal(true);
   isEditCategoryModalOpen = signal(true);
@@ -44,21 +49,53 @@ export class CategoriesComponent implements OnInit {
   constructor(
     private categoriesService: CategoriesService,
     private requiredValidator: RequiredValidator,
-    private minLengthValidator: MinLengthValidator
-  ) {}
-
-  ngOnInit(): void {
+    private minLengthValidator: MinLengthValidator,
+    private popupService: PopupService,
+  ) {
     this.loadCategories();
   }
 
   async loadCategories() {
-    const success = await this.categoriesService.getCategories();
-    if (!success?.data) {
-      console.error('Erro ao carregar categorias:', success?.data);
+    const response = await this.categoriesService.getCategories();
+  
+    if (!response?.data) {
+      this.popupService.addNewPopUp({
+        type: Status.Error,
+        message: 'Algo deu errado!',
+      });
       return;
     }
-    this.categories = Array.isArray(success.data) ? success.data : [success.data];
+  
+    if ('errors' in response.data) {
+      Object.values(response.data.errors).forEach((error) => {
+        this.popupService.addNewPopUp({
+          type: Status.Error,
+          message: error,
+        });
+      });
+      return;
+    }
+  
+    const categoriesList = response.data.data?.['deviceCategories'];
+  
+    if (Array.isArray(categoriesList)) {
+      const categories = categoriesList
+        .map(item => ({
+          id: Number(item['id']),
+          label: item['name'],
+          isActive: item['is_active']
+        }))
+        .filter(category => category.isActive);
+  
+      this.categories.set(categories.filter(category => category));
+    } else {
+      this.popupService.addNewPopUp({
+        type: Status.Error,
+        message: 'Formato inesperado da lista de categorias.',
+      });
+    }
   }
+  
 
   openNewCategoryModal() {
     this.isNewCategoryModalOpen.set(false);
@@ -89,40 +126,70 @@ export class CategoriesComponent implements OnInit {
   async addNewCategory() {
     if (!this.validateNewCategory()) return;
 
-    const success = await this.categoriesService.addCategory(this.newCategorieData().value);
-    if (!success?.data) {
-      console.error('Erro ao adicionar categoria:', success?.data);
-      return;
+    try {
+      const response = await this.categoriesService.addCategory(this.newCategorieData().value);
+      if (response && response.data) {
+        this.newCategorieData().value = '';
+        await this.loadCategories();
+        this.closeNewCategoryModal();
+      } else {
+        this.popupService.addNewPopUp({
+          type: Status.Error,
+          message: 'Erro ao adicionar a categoria.',
+        });
+      }
+    } catch (error) {
+      this.popupService.addNewPopUp({
+        type: Status.Error,
+        message: 'Erro inesperado ao adicionar a categoria.',
+      });
     }
-    this.loadCategories();
-    this.closeNewCategoryModal();
   }
 
   async updateCategory() {
     if (!this.validateEditedCategory()) return;
 
-    const success = await this.categoriesService.updateCategory(
-      this.selectedCategoryData().id,
-      this.selectedCategoryData().value
-    );
-    if (!success?.data) {
-      console.error('Erro ao atualizar categoria:', success?.data);
-      return;
+    try {
+      const response = await this.categoriesService.updateCategory(this.selectedCategoryData().id!, this.selectedCategoryData().value);
+      if (response && response.data) {
+        this.selectedCategoryData().value = '';
+        this.selectedCategoryData().id = -1;
+        await this.loadCategories();
+        this.closeEditCategoryModal();
+      } else {
+        this.popupService.addNewPopUp({
+          type: Status.Error,
+          message: 'Erro ao atualizar a categoria.',
+        });
+      }
+    } catch (error) {
+      this.popupService.addNewPopUp({
+        type: Status.Error,
+        message: 'Erro inesperado ao atualizar a categoria.',
+      });
     }
-    this.loadCategories();
-    this.closeEditCategoryModal();
   }
 
   async deleteCategory() {
     if (this.selectedCategoryData().id < 0) return;
 
-    const success = await this.categoriesService.removeCategory(this.selectedCategoryData().id);
-    if (!success?.data) {
-      console.error('Erro ao deletar categoria:', success?.data);
-      return;
+    try {
+      const response = await this.categoriesService.removeCategory(this.selectedCategoryData().id!);
+      if (response && response.data) {
+        await this.loadCategories();
+        this.closeDeleteCategoryModal();
+      } else {
+        this.popupService.addNewPopUp({
+          type: Status.Error,
+          message: 'Erro ao deletar a categoria.',
+        });
+      }
+    } catch (error) {
+      this.popupService.addNewPopUp({
+        type: Status.Error,
+        message: 'Erro inesperado ao deletar a categoria.',
+      });
     }
-    this.loadCategories();
-    this.closeDeleteCategoryModal();
   }
 
   validateNewCategory() {
--- frontend/src/app/features/employee/categories/service/categories.service.ts
+++ frontend/src/app/features/employee/categories/service/categories.service.ts
@@ -1,77 +1,80 @@
 import { Injectable } from '@angular/core';
 import axios, { AxiosResponse } from 'axios';
-import { Categorie } from '@/shared/types/categorie.type';
 import { ResponseError } from '@/shared/types/api/response-error.type';
 import { ResponseSuccess } from '@/shared/types/api/response-success.type';
 import axiosConfig from '@/axios.config';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class CategoriesService {
-  private apiUrl = '/api/employee/categories';
-
-  constructor() {}
-
-  async getCategories(categoryId?: number): Promise<AxiosResponse<Categorie[] | Categorie, ResponseError | ResponseSuccess> | null> {
+  async getCategories(): Promise<AxiosResponse<ResponseError | ResponseSuccess> | null> {
     try {
-      if (categoryId) {
-        const response = await axiosConfig(`${this.apiUrl}/${categoryId}`);
-        return response;
-      }
-      const response = await axiosConfig(this.apiUrl);
+      const response = await axiosConfig.get('/api/employee/categories');
       return response;
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
-        console.error('Erro da API:', error.response.data);
-        return null;
+        return error.response;
       } else {
-        console.error('Erro inesperado:', error);
+        console.error('Unexpected error:', error);
         throw error;
       }
     }
   }
 
-  async addCategory(categoryName: string): Promise<AxiosResponse<Categorie, ResponseError | ResponseSuccess> | null> {
+  async getIdByName(categoryId: number): Promise<AxiosResponse<ResponseError | ResponseSuccess> | null> {
     try {
-      const response = await axiosConfig.post(`${this.apiUrl}/add`, { name: categoryName });
+      const response = await axiosConfig.get(`/api/employee/categories/${categoryId}`);
       return response;
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
-        console.error('Erro da API:', error.response.data);
+        console.error('API Error:', error.response.data);
         return null;
       } else {
-        console.error('Erro inesperado:', error);
+        console.error('Unexpected error:', error);
         throw error;
       }
     }
   }
 
-  async removeCategory(categoryId: number): Promise<AxiosResponse<Categorie, ResponseError | ResponseSuccess> | null> {
+  async addCategory(categoryName: string): Promise<AxiosResponse<ResponseError | ResponseSuccess> | null> {
     try {
-      const response = await axiosConfig.put(`${this.apiUrl}/edit/${categoryId}`, { is_active: false });
+      const response = await axiosConfig.post('/api/employee/categories/add', { name: categoryName, is_active: true });
       return response;
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
-        console.error('Erro da API:', error.response.data);
+        console.error('API Error:', error.response.data);
         return null;
       } else {
-        console.error('Erro inesperado:', error);
+        console.error('Unexpected error:', error);
         throw error;
       }
     }
   }
 
-  async updateCategory(categoryId: number, categoryNewName: string): Promise<AxiosResponse<Categorie, ResponseError | ResponseSuccess> | null> {
+  async removeCategory(categoryId: number): Promise<AxiosResponse<ResponseError | ResponseSuccess> | null> {
     try {
-      const response = await axiosConfig.put(`${this.apiUrl}/edit/${categoryId}`, { name: categoryNewName });
+      const response = await axiosConfig.put(`/api/employee/categories/edit/status/${categoryId}`, { is_active: false });
       return response;
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
-        console.error('Erro da API:', error.response.data);
+        console.error('API Error:', error.response.data);
         return null;
       } else {
-        console.error('Erro inesperado:', error);
+        console.error('Unexpected error:', error);
+        throw error;
+      }
+    }
+  }
+
+  async updateCategory(categoryId: number, categoryNewName: string): Promise<AxiosResponse<ResponseError | ResponseSuccess> | null> {
+    try {
+      const response = await axiosConfig.put(`/api/employee/categories/edit/name/${categoryId}`, { name: categoryNewName });
+      return response;
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        console.error('API Error:', error.response.data);
+        return null;
+      } else {
+        console.error('Unexpected error:', error);
         throw error;
       }
     }
